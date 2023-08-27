0708387281df7eaa08b55addc23badfbaea5be57
eric <ericibrahim19@gmail.com>
2023-08-27 19:25:28 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	new file:   js/index.js
    	modified:   style.css

---
+++ js/index.js
@@ -0,0 +1,52 @@
+// console.log("validacion")
+
+// const inputEmail = document.querySelector("#email")
+
+// inputEmail.addEventListener("blur", (evento) => {
+//     validarEmail(evento.target)
+// })
+
+// function validarEmail(input) {
+//     const email = new Email (input.value);
+//     console.log(email)
+// }
+
+// function existe(email) {
+
+// }
+
+
+// function validarEmail(email) {
+//     const patron = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//     return patron.test(email);
+//   }
+  
+//   const direccionEmail = "juliansilva@gmail.com";
+//   if (validarEmail(direccionEmail)) {
+//     console.log("La dirección de correo electrónico es válida.");
+//   } else {
+//     console.log("La dirección de correo electrónico no es válida.");
+//   }
+  
+// const emailInput = document.querySelector("#email");
+// const validationMessage = document.getElementById("validation");
+
+// emailInput.addEventListener("input", function() {
+//   const email = emailInput.value;
+//   if (validarEmail(email)) {
+//     validationMessage.textContent = "La dirección de correo electrónico es válida.";
+//     validationMessage.style.color = "green";
+//   } else {
+//     validationMessage.textContent = "La dirección de correo electrónico no es válida.";
+//     validationMessage.style.color = "red";
+//   }
+// });
+
+// function validarEmail(email) {
+//   const patron = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//   return patron.test(email);
+// }
+
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+  
